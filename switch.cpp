339ea7d93b660ea268553fd29e53846bc6eaecf8
4D-24\koyo <a>
2026-09-08 13:32:44 +0900

    プロジェクト ファイルを追加します。

---
+++ switch.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+enum class EnemyState
+{
+    Idle,
+    Attacking,
+    Defending,
+    Fleeing
+};
+
+class Enemy
+{
+    EnemyState state;
+
+    void Update()
+    {
+        switch (state)
+        {
+        case EnemyState::Idle:
+            // Handle idle state
+            break;
+        case EnemyState::Attacking:
+            // Handle attacking state
+            break;
+        case EnemyState::Defending:
+            // Handle defending state
+            break;
+        case EnemyState::Fleeing:
+            // Handle fleeing state
+            break;
+        default:
+            // Handle unknown state
+            break;
+        }
+	}
+
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
